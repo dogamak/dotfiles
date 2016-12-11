50d01c5f40c279486dd83bf71d84191774586423
Mitja Karhusaari <monttu300@gmail.com>
2016-12-11 17:53:44 +0200

    Fix emacs install script and marmalade repo url.

--- emacs/install-packages.el
+++ emacs/install-packages.el
@@ -1,18 +1,23 @@
 (require 'package)
+
+;; It would be nice to have these loaded from ~/.emacs.d/init.el
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("maramalade" . "http://marmalade-repo.org/packages/"))
+
 (package-initialize)
 
 (setq dotfiles-package-list
       '(
 	company
 	racer
+	magit
 	rust-mode
 	solarized-theme
 	toml-mode
 	))
 
 (package-refresh-contents)
-(mapc #'(lambda (package)
-	  (unless (package-installed-p package)
-	    (package-install package)))
-      dotfiles-package-list)
-(save-buffers-kill-emacs)
+(dolist (package dotfiles-package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
